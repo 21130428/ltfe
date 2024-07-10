44b2c1d757f2a58e0ee4943d01c84793cb5f28d9
21130410 <21130410@st.hcmuaf.edu.vn>
2024-07-11 01:45:11 +0700

    add slider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.scss';
+import Sidebar from "./components/Slider/HeaderSlider"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header/Header";
-
+import store from "./store/store";
+import {Provider} from "react-redux";
 function App() {
-  return (
-      <div className="App">
-        <BrowserRouter>
-          <Header />
-        </BrowserRouter>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Header />
+                    <Sidebar/>
+                </BrowserRouter>
+            </Provider>
+        </div>
+    );
 }
 
 export default App;
