f1667c2d83b22fb83396766e3c9aacde9c1da13e
21130428 <21130428@st.hcmuaf.edu.vn>
2024-07-17 02:17:22 +0700

    create sidebar

--- src/store/sidebarSlice.js
+++ src/store/sidebarSlice.js
@@ -1,19 +1,23 @@
-import {createSlice} from '@reduxjs/toolkit'
-const initialState = {// trạng thái ban đầu
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
     isSidebarOn: false
 }
-const sidebarSlice   = createSlice({
-    name:'sidebar',
+
+const sidebarSlice = createSlice({
+    name: 'sidebar',
     initialState,
     reducers: {
-        setSidebarOn:(state) => {
+        setSidebarOn: (state) => {
             state.isSidebarOn = true;
         },
+
         setSidebarOff: (state) => {
             state.isSidebarOn = false;
         }
-    }
+    },
 });
+
 export const {setSidebarOn, setSidebarOff} = sidebarSlice.actions;
 export const getSidebarStatus = (state) => state.sidebar.isSidebarOn;
 export default sidebarSlice.reducer;
